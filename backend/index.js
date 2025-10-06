ae1ba2dfb075d2e5b91d88908679509e2e360464
viveksingh045 <vivekkumarsingh9450@gmail.com>
2025-10-06 11:11:30 +0530

    chore: initialize project with package.json and dependencies

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import connectDB from './db/dbConnect.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(3000,()=>{
+    connectDB();
+    console.log("Server is running on port 3000");
+});

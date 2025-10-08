4a59c09522f33de4add8f19ac9a10929a228c458
viveksingh045 <vivekkumarsingh9450@gmail.com>
2025-10-08 23:18:52 +0530

    feat: implement user signup functionality with validation and token generation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,31 @@
+import {User} from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
 export const signup = async (req, res) => {
-  res.send("Signup Controller");
+  const { name, email, password } = req.body;
+  try {
+    if(!name || !email || !password) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+    const user = new User({ name, email, password: hashedPassword, verificationToken, verificationExpiresAt: Date.now() + 3600000 });
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({ success: true, message: "User created successfully", 
+    user: { ...user._doc, password: undefined } });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 }
 
 export const login = async (req, res) => {
--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,7 @@ const app=express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-});
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 

7e58e4029c07f2a4bb6049eab3553b6d78f8c53a
viveksingh045 <vivekkumarsingh9450@gmail.com>
2026-02-04 17:53:28 +0530

    feat: enhance project structure and add frontend tooling
    - Updated package.json to include scripts for frontend and backend development, and added a script to run both concurrently.
    - Changed dependency versions to fixed versions for consistency.
    - Added ESLint configuration for the frontend to enforce coding standards and best practices.
    - Created PostCSS configuration for Tailwind CSS integration in the frontend.
    - Initialized Tailwind CSS configuration file for styling.

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 import express from 'express';
-import connectDB from './db/dbConnect.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
+
+
 import authRoutes from './routes/auth.route.js';
+import connectDB from './db/dbConnect.js';
 
 dotenv.config();
 
 const app=express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const ProtectedRoute = ({ children }) => {
 		return <Navigate to='/login' replace />;
 	}
 
-	if (!user?.isVerified) {
+	if (!user.isVerified) {
 		return <Navigate to='/verify-email' replace />;
 	}
 
@@ -33,7 +33,7 @@ const ProtectedRoute = ({ children }) => {
 const RedirectAuthenticatedUser = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
 
-	if (isAuthenticated && user?.isVerified) {
+	if (isAuthenticated && user.isVerified) {
 		return <Navigate to='/' replace />;
 	}
 
@@ -45,13 +45,15 @@ function App() {
 
 	useEffect(() => {
 		checkAuth();
-	}, []);
+	}, [checkAuth]);
 
 	if (isCheckingAuth) return <LoadingSpinner />;
 
 	return (
-		<div className='min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
-			
+		<div
+			className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'
+		>
 			<FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
 			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
 			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
@@ -65,7 +67,6 @@ function App() {
 						</ProtectedRoute>
 					}
 				/>
-
 				<Route
 					path='/signup'
 					element={
@@ -74,7 +75,6 @@ function App() {
 						</RedirectAuthenticatedUser>
 					}
 				/>
-
 				<Route
 					path='/login'
 					element={
@@ -83,9 +83,7 @@ function App() {
 						</RedirectAuthenticatedUser>
 					}
 				/>
-
 				<Route path='/verify-email' element={<EmailVerificationPage />} />
-
 				<Route
 					path='/forgot-password'
 					element={
@@ -103,10 +101,9 @@ function App() {
 						</RedirectAuthenticatedUser>
 					}
 				/>
-
+				{/* catch all routes */}
 				<Route path='*' element={<Navigate to='/' replace />} />
 			</Routes>
-
 			<Toaster />
 		</div>
 	);
